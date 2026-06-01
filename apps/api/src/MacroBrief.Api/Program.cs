a298d1d997ad9a2ec16106a7da9e8c57ee6371dd
kbyunghak <kbyunghak@gmail.com>
2026-06-01 03:37:50 -0700

    fix(api): enable CORS for local web client

--- apps/api/src/MacroBrief.Api/Program.cs
+++ apps/api/src/MacroBrief.Api/Program.cs
@@ -1,11 +1,22 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("WebClient", policy =>
+    {
+        policy
+            .WithOrigins("http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 builder.Services.AddSingleton<IHoldingsService, InMemoryHoldingsService>();
 builder.Services.AddSingleton<IMappingRulesProvider, JsonMappingRulesProvider>();
 builder.Services.AddSingleton<IPortfolioInsightsService, InMemoryPortfolioInsightsService>();
 builder.Services.AddSingleton<IFeedbackService, InMemoryFeedbackService>();
 
 var app = builder.Build();
+app.UseCors("WebClient");
 
 if (app.Environment.IsDevelopment())
 {
